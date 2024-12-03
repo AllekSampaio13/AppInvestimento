0bcdaa338bcd24951e1db50b0e34f6721e29b333
Állek Sampaio <alleksamp@gmail.com>
2024-12-03 15:52:40 -0300

    AppInvestimento
    Commit ajuste

---
+++ AppInvest.Domain/Interfaces/Repositories/IAcoesRepository.cs
@@ -0,0 +1,14 @@
+﻿using AppInvest.Domain.Models;
+
+
+namespace AppInvest.Domain.Interfaces.Repositories;
+
+public interface IAcoesRepository : IBaseRepository<Acoes>
+{
+    Task<Acoes> BuscarAcoesId(int Id);
+    Task<IEnumerable<Acoes>> BuscarAcoesNome(string nome);
+    Task CadastrarAcoes(Acoes acoes);
+    Task AtualizarAcoes(Acoes acoes);
+    Task<IEnumerable<Acoes>> BuscarAcoes();
+    Task DeletarAcoes(Acoes acoes);
+}
